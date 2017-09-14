a699bd64423c29b1291556e4885832810fb7caf9
Garreth Broesche <gbroesche@gmail.com>
2017-09-14 10:46:16 -0500

    added IDoDeclare.js

---
+++ JS/IDoDeclare/IDoDeclare.js
@@ -0,0 +1,32 @@
+var myFavoriteNumber = 25;
+console.log(myFavoriteNumber);
+
+var joseAltuvesNumber = 27;
+console.log(joseAltuvesNumber);
+
+var myAge = 44;
+console.log(myAge);
+
+var negativeNumber = -37;
+console.log(negativeNumber);
+
+var myName = 'Garreth';
+console.log(myName);
+
+var myWifesName = 'Lauren';
+console.log(myWifesName);
+
+var mySaying = 'The only thing better than a plan is a better plan';
+console.log(mySaying);
+
+var helloWorld = 'Hello World';
+console.log(helloWorld);
+
+var iAmCool = true;
+console.log(iAmCool);
+
+var theWorldIsFlat = false;
+console.log(theWorldIsFlat);
+
+var undefinedVariable;
+
