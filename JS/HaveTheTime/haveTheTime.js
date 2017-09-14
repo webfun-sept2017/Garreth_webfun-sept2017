db643b9afc541c0e2efab2ca0d8a5feb48c5bb22
Garreth Broesche <gbroesche@gmail.com>
2017-09-14 17:11:38 -0500

    added haveTheTime.js

---
+++ JS/HaveTheTime/haveTheTime.js
@@ -0,0 +1,19 @@
+var HOUR = 8;
+var MINUTE = 10;
+var PERIOD = "AM"
+
+var response = "It's ";
+
+if(MINUTE > 30){
+	str += "almost " + (HOUR + 1)
+}else{
+	str += "just after " + HOUR
+}
+
+if(PERIOD == "PM"){
+	str += " in the evening."
+else{
+	str += " in the morning."
+}
+
+console.log(response)
